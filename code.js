e4fdae652a65a8c43892732eae03041b66e20618
@Ignacioaj131982 <ignacioarevalo@gmail.com>
2024-05-18 18:16:29 -0300

    Parcial Front II terminado

--- code.js
+++ code.js
@@ -116,4 +116,13 @@ function alternarColorTema() {
 }
 
 /* --------------------- PUNTO 5: Escribe tu codigo aqui --------------------- */
+let miTecla = document.getElementById('sobre-mi');
+let oculto = document.querySelector('.oculto');
+document.addEventListener('keypress', function(event){
+  if (event.key === 'f') {
+    oculto.style.visibility = 'visible';
+
+  }
+
+});
 
